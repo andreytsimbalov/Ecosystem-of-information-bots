6c2bc2b7a332def4dca1ee2a317167eb17a05763
andreytsimbalov <andrey.tsimbalov@mail.ru>
2019-04-03 17:07:16 +0300

    potser on vk wall

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,36 @@
-print('hello world')
-print('test")
+# Приложение, по постингу текстовых постов с тегами в группу вк
+
+import vk_api
+import random
+
+def post2vk(letter): #ввод строки сообщения в группу
+    login = 'login@vk.ru'
+    password = 'password'
+    app_id = 6666666
+
+    vk_session = vk_api.VkApi(login=login
+                              ,password= password,app_id=app_id,scope='wall')
+    vk_session.auth()
+
+    vk1 = vk_session.get_api()
+
+    print(vk1.wall.post(owner_id='-123456789', message=str(letter)))
+
+
+#генератор текста
+def textgeneration(tags_num): #пример tags_num=[0,1,2,3,4,5,6,7,8,9]
+    letter = ''
+
+    for i in tags_num:
+        letter+='#tags'+str(i)+'\n'
+
+    letter+='\n'
+    letter+='Random text №'+str(int(random.random()*1000))+'.'
+
+    return letter
+
+# tags_num=[0,1,2,3,4,5,6,7,8,9] #пример работы программы
+# letter=textgeneration(tags_num)
+# print(letter)
+#
+# post2vk(letter)
